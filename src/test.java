d413213c1e1a9345036528c55b4e5d888284df4a
wancheol <tlsdhks543@naver.com>
2024-05-31 21:26:03 +0900

    wancheol

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("깃 줮같네 진짜");
         System.out.println("변경하였습니다.");
+        System.out.println("commit");
     }
 }
