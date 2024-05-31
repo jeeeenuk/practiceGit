a7a3123155d3ae2064458be514740f578c24da28
JeenukChung <jeenuk.chung@gmail.com>
2024-05-31 14:13:04 +0900

    test A 변경

--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("씨발 테스트다");
+        System.out.println("깃 줮같네 진짜");
+        System.out.println("변경하였습니다.");
     }
 }
